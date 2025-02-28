9562d9f3d09783dbcbeba29c796faef5b87fb72f
Josue Ortiz <156556659+JosueOrt04@users.noreply.github.com>
2025-02-27 21:49:20 -0600

    Add files via upload

---
+++ js/plans.js
@@ -0,0 +1,7 @@
+// Función para animar los planes cuando se presionan
+function animatePlan(plan) {
+    plan.classList.add("clicked");
+    setTimeout(() => {
+        plan.classList.remove("clicked");
+    }, 200);
+}
